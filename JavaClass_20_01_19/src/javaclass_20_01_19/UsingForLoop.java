3b4f91135e9bb46c17eb9445d63d7a183e04daa0
arfin <md.arfinshekh2017@gmail.com>
2019-01-20 18:55:37 +0600

    javaclass

---
+++ JavaClass_20_01_19/src/javaclass_20_01_19/UsingForLoop.java
@@ -0,0 +1,11 @@
+package javaclass_20_01_19;
+
+public class UsingForLoop {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ JavaClass_20_01_19/src/javaclass_20_01_19/UsingWhileLoop.java
@@ -0,0 +1,12 @@
+package javaclass_20_01_19;
+
+public class UsingWhileLoop {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 20);
+    }
+}
