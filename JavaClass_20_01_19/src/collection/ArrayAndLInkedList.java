775db16ea3f2d431e2923d3fb1ead311f0f5072d
arfin <md.arfinshekh2017@gmail.com>
2019-01-21 18:46:15 +0600

    javaclass

---
+++ JavaClass_20_01_19/src/collection/ArrayAndLInkedList.java
@@ -0,0 +1,69 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ArrayAndLInkedList {
+
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        LinkedList linkedList = new LinkedList();
+
+        //ArrayList add
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i <= 100000; i++) {
+            arrayList.add(i);
+        }
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("ArrayList add: " + duration);
+
+        //LinkedList ass
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i <= 100000; i++) {
+            linkedList.add(i);
+        }
+        endTime = System.currentTimeMillis();
+        duration = endTime - startTime;
+        System.out.println("LinkedList add: " + duration);
+
+        //ArrayList get
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i <= 10000; i++) {
+            arrayList.get(i);
+        }
+        endTime = System.currentTimeMillis();
+        duration = endTime - startTime;
+        System.out.println("ArrayList get: " + duration);
+
+        //LinkedList get
+        startTime = System.currentTimeMillis();
+
+        for (int i = 0; i <= 10000; i++) {
+            linkedList.get(i);
+        }
+        endTime = System.currentTimeMillis();
+        duration = endTime - startTime;
+        System.out.println("LinkedList get: " + duration);
+
+        //ArrayList remove
+        startTime = System.currentTimeMillis();
+
+        for (int i = 9999; i >= 0; i--) {
+            arrayList.remove(i);
+        }
+        endTime = System.currentTimeMillis();
+        duration = endTime - startTime;
+        System.out.println("ArrayList remove: " + duration);
+
+        //LinkedList remove
+        startTime = System.currentTimeMillis();
+
+        for (int i = 9999; i >= 0; i--) {
+            linkedList.remove(i);
+        }
+        endTime = System.currentTimeMillis();
+        duration = endTime - startTime;
+        System.out.println("LinkedList remove: " + duration);
+    }
+}
